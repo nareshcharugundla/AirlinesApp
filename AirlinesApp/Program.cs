df070f26474e3f25286e65d460e52f878f38eda5
Naresh Charugundla <nachar@microsoft.com>
2020-01-18 19:37:46 -0800

    Selection

--- AirlinesApp/Program.cs
+++ AirlinesApp/Program.cs
@@ -7,6 +7,9 @@ namespace AirlinesApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Airlines. Please select an option below");
+            Console.WriteLine("1. Search Flights\n" +
+                              "2. Manage Booking\n");
+            int selection = int.Parse(Console.ReadLine());
 
         }
     }
