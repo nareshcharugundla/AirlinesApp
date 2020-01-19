d5d701326a0239930fa78ae75c5f6da28896d7d1
Naresh Charugundla <nachar@microsoft.com>
2020-01-18 18:17:52 -0800

    Creating new Airlines Project and class.

---
+++ AirlinesApp/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlinesApp
+{
+    class Flight
+    {
+        #region Flight Class Properties
+        public int FlightNumber { get; set; }
+        public string FlightStartAirport { get; set; }
+        public string FlightDestinationAirport { get; set; }
+        public int FlightCapacity { get; set; }
+        #endregion
+    }
+}
