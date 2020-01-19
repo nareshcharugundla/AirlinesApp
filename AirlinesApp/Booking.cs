03e150d66e3bf7f28e030a953dc4c6cd433c61df
Naresh Charugundla <nachar@microsoft.com>
2020-01-18 18:48:17 -0800

    Creating Booking Class

---
+++ AirlinesApp/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlinesApp
+{
+    class Booking
+    {
+        public string BookingID { get; set; }
+        public int FlightNumber { get; set; }
+        public string PassengerName { get; set; }
+        public string DepartingAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+    }
+}
